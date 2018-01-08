ce1a95b3393a84b398b526b0a2419443bc204de3
KyleDgls <kyle.dgls@gmail.com>
2018-01-07 18:41:40 -0800

    read me

--- Loops/loopsapp/views.py
+++ Loops/loopsapp/views.py
@@ -2,17 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .secrets import google_maps_api_key
 import json
+import requests
 
 # Create your views here.
 def index(request):
     print(google_maps_api_key)
     return render(request, 'loopsapp/index.html', {'google_maps_api_key':google_maps_api_key})
 
+
 def getloop(request):
     data = json.loads(request.body)
-    print(data['locations'])
+#    print(data['locations'])
+    locations = data['locations']
+    first = locations.pop(0)
+    url = 'https://maps.googleapis.com/maps/api/directions/json?'
+    url += 'key=' + google_maps_api_key
+    return HttpResponse(url)
 
 
-    return HttpResponse('test123')
+    # return HttpResponse(json.dumps(data['locations']))
+
 
 
