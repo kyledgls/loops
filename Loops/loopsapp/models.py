556d5557662dcfefdcdd8c46c7094f7d3494de66
KyleDgls <kyle.dgls@gmail.com>
2018-01-09 15:04:31 -0800

    read me

--- Loops/loopsapp/models.py
+++ Loops/loopsapp/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
 
 
+
+class User(models.Model):
+    first = models.CharField(max_length=50)
+    last = models.CharField(max_length=50)
+    email = models.CharField(max_length=250)
+
+
+class loop(models.Model):
+    email = models.CharField(max_length=250)
+    name_search = models.CharField(max_length=20)
+    json = models.TextField()
